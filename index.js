67bf68d68942b864cc565172fc2012c1baf4d8e0
Mirzaev Orzu <no20020205@gmail.com>
2021-08-13 09:09:20 +0500

    add get files route, remove middleware

--- index.js
+++ index.js
@@ -4,50 +4,40 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/files", validate, (req, res) => {
-    const { filename, content } = req.body;
+  const { filename, content } = req.body;
 
-    fs.writeFile(`./files/${filename}`, content,  err => {
-        if (err) {
-        console.log(err.message);
-           res.status(500).json({
-            "message": "Server error"
-          })
-        } else {
-            res.status(200).json({
-                message: "File created successfully",
-            })
-        }
-    })
-})
-
-function validate(req, res, next) {
-    const {filename, content} = req.body;
-    console.log(req.body);
-    const allowedExtentions = ["log", "txt", "json", "yaml", "xml", "js"];
-
-    if(filename === undefined) {
-        res.status(400).json({
-            message:  "Please specify 'filename' parameter"
-        })
-    } else if (content === undefined) {
-        res.status(400).json({
-            message:  "Please specify 'content' parameter"
-        })
+  fs.writeFile(`./files/${filename}`, content, (err) => {
+    if (err) {
+      res.status(500).json({
+        message: "Server error",
+      });
     } else {
-        let extention = filename.slice(filename.lastIndexOf(".") + 1);
-        if(!allowedExtentions.includes(extention)) {
-            res.status(400).json({
-                message: `File extention ${extention} is not allowed`
-            })
-        }
-        next();
+      res.status(200).json({
+        message: "File created successfully",
+      });
     }
-}
+  });
+});
+
+app.get("/api/files", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      res.status(500).json({
+        message: "Server error",
+      });
+    } else {
+      res.status(200).json({
+        message: "success",
+        files,
+      });
+    }
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+  console.log(`Listening on port ${PORT}`);
+});
